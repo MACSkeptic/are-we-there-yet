cb43a16ebdce1a445fd1a4bee1e1e1f6682e715e
mozair <macskeptic@gmail.com>
2016-08-11 08:50:32 +0200

    interval options

--- inject.js
+++ inject.js
@@ -1,57 +1,66 @@
+function anybody() {
+  var url = `${location.href}`;
+  var branch = document.querySelector('.commit-ref.current-branch.head-ref').innerText;
+  var lastsha = document.querySelector('tr.commit:last-of-type .commit-meta').innerText;
+  return `url: ${url}\nbranch: ${branch}\nlastsha: ${lastsha}`;
+}
 function signalSuccess() {
   document.body.setAttribute('grumpy-notified', 'success');
   new Notification('w00t \\o/', {
     icon: chrome.extension.getURL('green-grumpy.png'),
-    body: document.querySelector('.commit-ref.current-branch.head-ref').innerText
+    body: anybody()
   });
 }
 function signalError() {
   document.body.setAttribute('grumpy-notified', 'error');
   new Notification('o noes /o\\', {
     icon: chrome.extension.getURL('artsy-grumpy.png'),
-    body: document.querySelector('.commit-ref.current-branch.head-ref').innerText
+    body: anybody()
   });
 }
 function signalProgress() {
   document.body.setAttribute('grumpy-notified', 'progress');
   new Notification('waiting...', {
     icon: chrome.extension.getURL('blue-grumpy.png'),
-    body: document.querySelector('.commit-ref.current-branch.head-ref').innerText
+    body: anybody()
   });
 }
-setInterval(() => {
-  Notification.requestPermission(() => {
-    chrome.storage.sync.get('are-we-there-yet--patterns', (items) => {
-      var patterns = (items['are-we-there-yet--patterns'] || '').split(',');
-      var url = `${location.href}`;
-      var lastUrl = document.body.getAttribute('grumpy-url');
-      document.body.setAttribute('grumpy-url', url);
-      if (![].find.call(patterns, pattern => ((new RegExp(pattern)).test(url)))) {
-        if (lastUrl !== url) {
-          console.debug(
-            lastUrl, '=>', url,
-            'this url does not match any patterns,',
-            'include it in the extension options if necessary.',
-            'of course, it uses regular expressions'
-          );
+chrome.storage.sync.get('are-we-there-yet--interval', (items) => {
+  var interval = +items['are-we-there-yet--interval'] || 2000;
+  setInterval(() => {
+    Notification.requestPermission(() => {
+      chrome.storage.sync.get('are-we-there-yet--patterns', (items) => {
+        var patterns = (items['are-we-there-yet--patterns'] || '').split(',');
+        var url = `${location.href}`;
+        var lastUrl = document.body.getAttribute('grumpy-url');
+        document.body.setAttribute('grumpy-url', url);
+        if (![].find.call(patterns, pattern => ((new RegExp(pattern)).test(url)))) {
+          if (lastUrl !== url) {
+            console.debug(
+              lastUrl, '=>', url,
+              'this url does not match any patterns,',
+              'include it in the extension options if necessary.',
+              'of course, it uses regular expressions'
+            );
+          }
+          return;
         }
-        return;
-      }
-      if (lastUrl !== url) {
-        console.debug(lastUrl, '=>', url, 'starting notifications, you can customize these with regular expressions');
-        document.body.setAttribute('grumpy-notified', '');
-      }
-      var element = document.querySelector('.branch-action-item-icon.completeness-indicator');
-      if (!element) {
-        console.warn('could not find branch status element, is this a PR page?');
-      }
-      var lastNotified = document.body.getAttribute('grumpy-notified');
-      var currentStatus = (element.classList.contains('completeness-indicator-success') && 'success') ||
-        (element.classList.contains('completeness-indicator-error') && 'error') ||
-        'progress';
-      console.debug(lastNotified, '=>', currentStatus);
-      var action = { success: signalSuccess, error: signalError, progress: signalProgress }[currentStatus];
-      (lastNotified !== currentStatus) && action && action();
+        if (lastUrl !== url) {
+          console.debug(lastUrl, '=>', url, 'starting notifications, you can customize these with regular expressions');
+          document.body.setAttribute('grumpy-notified', '');
+        }
+        var element = document.querySelector('.branch-action-item-icon.completeness-indicator');
+        if (!element) {
+          console.warn('could not find branch status element, is this a PR page?');
+        }
+        var lastNotified = document.body.getAttribute('grumpy-notified');
+        var currentStatus = (element.classList.contains('completeness-indicator-success') && 'success') ||
+          (element.classList.contains('completeness-indicator-error') && 'error') ||
+          'progress';
+        console.debug(lastNotified, '=>', currentStatus);
+        var action = { success: signalSuccess, error: signalError, progress: signalProgress }[currentStatus];
+        (lastNotified !== currentStatus) && action && action();
+      });
     });
-  });
-}, 5000);
+  }, interval);
+});
--- options.js
+++ options.js
@@ -1,12 +1,19 @@
-chrome.storage.sync.get('are-we-there-yet--patterns', (items) => {
-  document.querySelector('[name="patterns"]').value = items['are-we-there-yet--patterns'];
+chrome.storage.sync.get(['are-we-there-yet--patterns', 'are-we-there-yet--interval'], (items) => {
+  document.querySelector('[name="patterns"]').value = items['are-we-there-yet--patterns'] || 'pull/([0-9]+)';
+  document.querySelector('[name="interval"]').value = items['are-we-there-yet--interval'] || 2000;
   document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
+    var interval = document.querySelector('[name="interval"]').value || '';
+    var patterns = document.querySelector('[name="patterns"]').value || '';
     chrome.storage.sync.set({
-      'are-we-there-yet--patterns': document.querySelector('[name="patterns"]').value || ''
+      'are-we-there-yet--interval': `${interval}`,
+      'are-we-there-yet--patterns': `${patterns}`
     }, () => {
       Notification.requestPermission(() => {
-        new Notification(`saved!`);
+        new Notification(`saved!`, {
+          icon: chrome.extension.getURL('happy-grumpy.png'),
+          body: `using:\n - interval: ${interval}\n - patterns:\n   - ${patterns.split(',').join('\n   - ')}`
+        });
       });
     });
   });
