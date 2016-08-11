6d7833cfdd7d97282d2267bf27edc54e5a650c6a
mozair <macskeptic@gmail.com>
2016-08-11 08:28:56 +0200

    it works

--- inject.js
+++ inject.js
@@ -1,25 +1,57 @@
-chrome.storage.sync.get('are-we-there-yet--patterns', (items) => {
-  var patterns = (items['are-we-there-yet--patterns'] || '').split(',');
-  if ([].find.call(patterns, pattern => `${location.href}`.includes(pattern))) {
-    setInterval(() => {
-      Notification.requestPermission(() => {
-        var node = document.querySelector('.branch-action-body .status-heading');
-        var text = node.innerText;
-        var notified = node.getAttribute('notified');
-        if (notified) {
-          return;
+function signalSuccess() {
+  document.body.setAttribute('grumpy-notified', 'success');
+  new Notification('w00t \\o/', {
+    icon: chrome.extension.getURL('green-grumpy.png'),
+    body: document.querySelector('.commit-ref.current-branch.head-ref').innerText
+  });
+}
+function signalError() {
+  document.body.setAttribute('grumpy-notified', 'error');
+  new Notification('o noes /o\\', {
+    icon: chrome.extension.getURL('artsy-grumpy.png'),
+    body: document.querySelector('.commit-ref.current-branch.head-ref').innerText
+  });
+}
+function signalProgress() {
+  document.body.setAttribute('grumpy-notified', 'progress');
+  new Notification('waiting...', {
+    icon: chrome.extension.getURL('blue-grumpy.png'),
+    body: document.querySelector('.commit-ref.current-branch.head-ref').innerText
+  });
+}
+setInterval(() => {
+  Notification.requestPermission(() => {
+    chrome.storage.sync.get('are-we-there-yet--patterns', (items) => {
+      var patterns = (items['are-we-there-yet--patterns'] || '').split(',');
+      var url = `${location.href}`;
+      var lastUrl = document.body.getAttribute('grumpy-url');
+      document.body.setAttribute('grumpy-url', url);
+      if (![].find.call(patterns, pattern => ((new RegExp(pattern)).test(url)))) {
+        if (lastUrl !== url) {
+          console.debug(
+            lastUrl, '=>', url,
+            'this url does not match any patterns,',
+            'include it in the extension options if necessary.',
+            'of course, it uses regular expressions'
+          );
         }
-        if ((/all checks have passed/i).test(text)) {
-          node.setAttribute('notified', true);
-          new Notification('all green');
-          return;
-        }
-        if ((/failed/i).test(text)) {
-          node.setAttribute('notified', true);
-          new Notification('o noes!');
-          return;
-        }
-      });
-    }, 5000);
-  }
-});
+        return;
+      }
+      if (lastUrl !== url) {
+        console.debug(lastUrl, '=>', url, 'starting notifications, you can customize these with regular expressions');
+        document.body.setAttribute('grumpy-notified', '');
+      }
+      var element = document.querySelector('.branch-action-item-icon.completeness-indicator');
+      if (!element) {
+        console.warn('could not find branch status element, is this a PR page?');
+      }
+      var lastNotified = document.body.getAttribute('grumpy-notified');
+      var currentStatus = (element.classList.contains('completeness-indicator-success') && 'success') ||
+        (element.classList.contains('completeness-indicator-error') && 'error') ||
+        'progress';
+      console.debug(lastNotified, '=>', currentStatus);
+      var action = { success: signalSuccess, error: signalError, progress: signalProgress }[currentStatus];
+      (lastNotified !== currentStatus) && action && action();
+    });
+  });
+}, 5000);
